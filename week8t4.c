42634d52ba91762ef11e67db2a18c770bdf8da1c
Bunke <2114347409@qq.com>
2020-04-14 17:43:49 +0800

    修改

--- week8t4.c
+++ week8t4.c
@@ -2,74 +2,64 @@
 #include <stdlib.h>
 #define QUEUE_SIZE 4
 
-typedef struct elem{
-    char content;
-    int tag;
-}Elem;
-
-typedef struct queue {
-    Elem elem[QUEUE_SIZE];
+typedef struct {
+    int elem[QUEUE_SIZE];
     int front;
     int rear;
+    int tag;
 } Queue;
 
-int initQueue(Queue *queue){//初始化,把所有元素都标记为0
-    int i;
-    for (i = 0; i < QUEUE_SIZE; i++) {
-        queue->elem[i].tag = 0;
-    }
+int initQueue(Queue *queue) {
+    queue->tag = 0;
     queue->front = 0;
     queue->rear = 0;
+    return 1;
 }
 
-int enQueue(Queue *queue, char en) {//添加元素
-    int a;
-    a = queue->rear;
-    if (queue->elem[a].tag == 0) {//判断下一个是否标记是否为0,为0则允许添加
-        queue->elem[a].tag = 1;
-        queue->elem[a].content = en;
-        if (++queue->rear >= QUEUE_SIZE) {//判断指针是否超过容量
-            queue->rear %= QUEUE_SIZE;
-        }
+int enQueue(Queue *queue, int a) {
+    if (queue->front == queue->rear && queue->tag == 1) {
+        return 0;
+    }
+    queue->elem[queue->rear++] = a;
+    printf("%d  ", a);
+    if (queue->rear >= QUEUE_SIZE) {
+        queue->rear %= QUEUE_SIZE;
+    }
+    if (queue->front == queue->rear) {
+        printf("Stack filled\n");
+        queue->tag = 1;
     }
     return 1;
 }
 
-int deQueue(Queue *queue, char *de) {
-    int a;
-    a = queue->front;
-    if (queue->elem[a].tag == 1) {//判断当前是否标记为1,为1则允许弹出
-        queue->elem[a].tag = 0;
-        *de = queue->elem[a].content;
-        if (++a >= QUEUE_SIZE) {//判断是否超过容量
-            a %= QUEUE_SIZE;
-        }
-        queue->front = a;
+int deQueue(Queue *queue,int *de){
+    if (queue->front == queue->rear && queue->tag == 0) {
+        return 0;
+    }
+    *de = queue->elem[queue->front++];
+    printf("%d  ", *de);
+    if (queue->front >= QUEUE_SIZE) {
+        printf("Stack empty\n");
+        queue->front %= QUEUE_SIZE;
+    }
+    if (queue->front == queue->rear) {
+        queue->tag = 0;
     }
     return 1;
 }
-
 int main(){
+    int a;
     Queue queue;
-    char a = '\0';
     initQueue(&queue);
-    enQueue(&queue, 'w');
-    enQueue(&queue, 'e');
-    enQueue(&queue, 'a');
-    enQueue(&queue, 'r');
-    enQueue(&queue, 'e');
+    enQueue(&queue, 1);
+    enQueue(&queue, 2);
+    enQueue(&queue, 3);
+    enQueue(&queue, 4);
+    enQueue(&queue, 5);
     deQueue(&queue, &a);
-    printf("%c\n", a);
-    a = '\0';
     deQueue(&queue, &a);
-    printf("%c\n", a);
-    a = '\0';
     deQueue(&queue, &a);
-    printf("%c\n", a);
-    a = '\0';
     deQueue(&queue, &a);
-    printf("%c\n", a);
-    a = '\0';
     deQueue(&queue, &a);
-    printf("%c\n", a);
+    return 1;
 }
